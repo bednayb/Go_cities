a5a6ec0852b62315e3b2f18815ebd7f7c03247a2
Bence <bence@Bences-iMac.local>
2017-07-05 14:49:42 +0200

    add CityInfo struct

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/go-martini/martini"
+	"time"
+
+)
+
+
+type CityInfo struct {
+	City string
+	Geo map[string]float64
+	Temp [5]float64
+	Rain [5]float64
+    Timestamp time.Time
+
+}
+
 
 func main() {
 	m := martini.Classic()
 	m.Get("/", func() string {
 		return "Hello world!"
 	})
+
+
 	m.Run()
 }
